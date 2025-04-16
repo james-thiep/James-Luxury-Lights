ca6d5f03c368162b93643af16bd3a59b99cb1753
james-thiep <thiepjames8@gmail.com>
2025-04-16 12:27:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any JavaScript functionality you need here
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: Add a simple alert when the page loads
+    alert('Welcome to James Luxury Lights!');
+});
